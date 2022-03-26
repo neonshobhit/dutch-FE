bc14a5c8a83543834c3b48a5c200bc660296bde1
code123-tech <swapnilagarwal2001@gmail.com>
2022-03-26 22:13:37 +0530

    Member added using email

--- src/js/event-details.js
+++ src/js/event-details.js
@@ -6,6 +6,8 @@ const jwtToken = localStorage.getItem("jwtToken")
 	: "";
 
 const eventName = document.querySelector(`.event-name input[type="text"]`);
+const addMemberForm = document.getElementById("add-member-form");
+const errorMessage = document.getElementById("errorMessage");
 
 function toggleModal() {
 	modal.classList.toggle("show-modal");
@@ -14,6 +16,11 @@ function windowOnClick(event) {
 	if (event.target === modal) {
 		toggleModal();
 	}
+}
+function validateEmail(email) {
+	const regex = properties.EMAIL_REGEX;
+
+	return regex.test(email.trim().toLowerCase());
 }
 
 const addUser = () => {
@@ -53,12 +60,20 @@ const generateMember = (member) => {
 };
 
 const displayMembers = (members) => {
+	document.getElementsByClassName("event-members-list")[0].innerHTML = "";
 	for (let i = 0; i < members.length; ++i) {
 		document
 			.getElementsByClassName("event-members-list")[0]
 			.insertAdjacentHTML("beforeend", generateMember(members[i]));
 	}
 };
+
+const getEventIdFromParams = () => {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	return urlParams.get("eventId").substring(6);
+};
+
 const getEvent = async (eventId) => {
 	let data = await fetch(`${properties.LOCAL}/events/display`, {
 		method: "POST",
@@ -75,12 +90,24 @@ const getEvent = async (eventId) => {
 		displayMembers(data.data.members);
 	}
 };
-
-const getEventIdFromParams = () => {
-	const queryString = window.location.search;
-	const urlParams = new URLSearchParams(queryString);
-	return urlParams.get("eventId").substring(6);
+const addNewMemberInList = async (addMemberData) => {
+	let data = await fetch(`${properties.LOCAL}/events/addmembers`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(addMemberData),
+	});
+	data = await data.json();
+	if (data && data.statusCode === 200) {
+		alert("Member added successfully");
+		toggleModal();
+		displayMembers(data.members);
+	} else {
+		alert(data.message);
+	}
 };
+
 window.addUser = addUser;
 window.copyInviteLink = copyInviteLink;
 window.editName = editName;
@@ -89,6 +116,30 @@ window.initialize = initialize;
 window.addExpenses = addExpenses;
 window.addEventListener("click", windowOnClick);
 closeButton.addEventListener("click", toggleModal);
+
+addMemberForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	let emailInput = document.getElementById("email");
+	let email = emailInput.value;
+	if (email.length === 0 || !validateEmail(email)) {
+		errorMessage.classList.remove("hide");
+		errorMessage.classList.add("show");
+		emailInput.classList.add("input-border-error");
+		errorMessage.innerText = "Please enter valid email";
+	} else {
+		errorMessage.classList.remove("show");
+		errorMessage.classList.add("hide");
+		emailInput.classList.remove("input-border-error");
+		let name = email.split("@")[0];
+		let data = {
+			eventId: getEventIdFromParams(),
+			memberEmail: email,
+			memberName: name.charAt(0).toUpperCase() + name.substring(1),
+			isGuest: false,
+		};
+		addNewMemberInList(data);
+	}
+});
 
 function initialize() {
 	if (!jwtToken) {
